152299132f2e91f93b26d0508f8e5cfa9034cd3a
karthik419914 <132698207+karthik419914@users.noreply.github.com>
2024-10-12 22:22:07 +0530

    trail-12
    added scroll track bar,
    scroll zoom down header,
    no text copy

--- scripts.js
+++ scripts.js
@@ -1,23 +1,27 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2024 00:00:00").getTime();
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {scrollFunction(),myFunction()};
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+// snackbar
 
-// Update the count down every 1 second
-var countdownfunction = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-    
-    if (distance < 0) {
-        clearInterval(countdownfunction);
-        document.getElementById("countdown-timer").innerHTML = "EXPIRED";
-    }
-}, 1000);
+function snackbar() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80|| document.documentElement.scrollTop > 80) {
+    document.getElementById("rev_header").style.fontSize = "26px";
+  } else {
+    document.getElementById("rev_header").style.fontSize = "40px";
+  }
+}
