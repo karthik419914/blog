74848b06fefc28ec7e0c3fc65ab26127dc97fc08
karthik419914 <132698207+karthik419914@users.noreply.github.com>
2025-11-06 19:26:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -138,6 +138,6 @@ window.addEventListener("load", function() {
             document.getElementById("loadingScreen").style.display = 'none'; // Hide loading screen
             document.getElementById("mainContent").style.display = 'block'; // Show main content
         }, 1000); // Match the CSS transition duration (1 second)
-    }, 4000); // set the delay here 
+    }, 2000); // set the delay here 
 });
 
